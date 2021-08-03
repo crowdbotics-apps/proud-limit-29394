9b542435adaddd33a6ae653814c357ef606c71ba
Crowdbotics <team@crowdbotics.com>
2021-08-03 13:42:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_limit_29394Identifier",
+  url: "https://proud_limit_29394Identifier.botics.co",
+  api: "https://proud_limit_29394Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
